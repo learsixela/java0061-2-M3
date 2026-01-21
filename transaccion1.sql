97a0c0b1e5d508c6aab7d726fb7cd1af5f721c89
Israel Palma <ipalm001@gmail.com>
2026-01-21 09:06:15 -0300

    Nuevo dump

--- transaccion1.sql
+++ transaccion1.sql
@@ -3,15 +3,16 @@ use java0062;
 START TRANSACTION;
 
 update usuario 
-set saldo =  saldo - 100 
-where id = 6;
+set saldo =  saldo - 400 
+where id = 1;
 
 update usuario 
-set saldo =  saldo + 100 
-where id = 1;
+set saldo =  saldo + 400 
+where id = 10;
+-- ROLLBACK;
 
 INSERT INTO transacciones
 (remitente_usuario_id, destinatario_usuario_id, importe) 
-VALUES ('6', '1', '100');
-
+VALUES ('1', '10', '400');
+ROLLBACK;
 COMMIT;
